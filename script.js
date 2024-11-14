22845649d541a1126c0bb51dc55294ec884c1cd9
Jiwoo Choi <jiwooch13@gmail.com>
2024-11-14 12:46:13 +0100

    Add the remaining styles and create the removeBook function

--- script.js
+++ script.js
@@ -18,10 +18,40 @@ function addBookToLibrary() {
     renderBooks();
 }
 
+function renderBooks() {
+    let libraryElement = document.querySelector("#library");
+    libraryElement.textContent= "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+        let bookElement = document.createElement("div");
+        bookElement.classList.add("book-card");
+        bookElement.innerHTML = `
+        <div class="card-header">
+            <h3 class="title">${book.title}</h3>
+            <h5 class="author">by ${book.author}</h5>
+        </div>
+
+        <div class="card-body">
+            <p>${book.pages}</p>
+            <p class="read-status">
+                ${book.read ? "Read" : "Not Read Yet"}
+            </p>
+            <button class="remove-button" onclick="removeBook(${i})">Remove Book</button>
+            <button class="toggle-read-button" onclick="toggleRead(${i})"></button>
+        </div>
+        `
+        libraryElement.appendChild(bookElement);
+    }
+}
+
+function removeBook(book) {
+    myLibrary.splice(book, 1);
+    renderBooks();
+}
+
 let newBookButton = document.querySelector("#new-book-button");
 newBookButton.addEventListener("click", () => {
     let newBookForm = document.querySelector("#new-book-form");
-    console.log(newBookForm);
     newBookForm.style.display = "block";
 });
 
